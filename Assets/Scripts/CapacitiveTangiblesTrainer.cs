d9ef21549d7fe4909388039384a4f8dea10f0152
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2018-05-29 19:49:13 +0200

    added json file save

--- Assets/Scripts/CapacitiveTangiblesTrainer.cs
+++ Assets/Scripts/CapacitiveTangiblesTrainer.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class CapacitiveTangiblesTrainer : MonoBehaviour {
 
+
+    public Text patternID;
     RectTransform rectTransform;
 	void Start () {
         rectTransform = transform as RectTransform;
@@ -12,13 +15,44 @@ public class CapacitiveTangiblesTrainer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
+	}
+
+
+    public void SavePattern() {
+        List<Vector2> patternPoints = new List<Vector2>();
         Touch[] touches = Input.touches;
-        foreach(Touch touch in touches){
+        foreach (Touch touch in touches)
+        {
             Vector2 pos = touch.position;
-            if(RectTransformUtility.RectangleContainsScreenPoint(rectTransform, pos)){
-                GetComponent<Image>().color = Color.red;
+            if (RectTransformUtility.RectangleContainsScreenPoint(rectTransform, pos))
+            {
+                patternPoints.Add(pos);
             }
         }
-        //
-	}
+
+        TangiblePattern pattern = new TangiblePattern();
+        pattern.id = patternID.text;
+
+        //List<string> strPoints = new List<string>();
+        //foreach(Vector2 p in patternPoints){
+        //    strPoints.Add(JsonMapper.ToPrettyJson(p));
+        //}
+
+        pattern.points = patternPoints;
+
+
+        string json = JsonUtility.ToJson(pattern, true);
+        string fullfilepath = PatternFilename(patternID.text);
+        print(fullfilepath);
+        System.IO.File.WriteAllText(fullfilepath, json);
+    }
+
+
+    public string PatternFilename(string patternID){
+        string filename = "tangible_" + patternID + ".json";
+        string fullpath = Application.persistentDataPath + "/" + filename;
+        return fullpath;
+    } 
+
 }
