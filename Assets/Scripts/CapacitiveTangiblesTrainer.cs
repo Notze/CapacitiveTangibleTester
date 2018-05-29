eb4efcc77c3b16cecebe02b55afc2702399f6a1c
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2018-05-29 19:07:21 +0200

    added trainer class

---
+++ Assets/Scripts/CapacitiveTangiblesTrainer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CapacitiveTangiblesTrainer : MonoBehaviour {
+
+    RectTransform rectTransform;
+	void Start () {
+        rectTransform = transform as RectTransform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Touch[] touches = Input.touches;
+        foreach(Touch touch in touches){
+            Vector2 pos = touch.position;
+            if(RectTransformUtility.RectangleContainsScreenPoint(rectTransform, pos)){
+                GetComponent<Image>().color = Color.red;
+            }
+        }
+        //
+	}
+}
