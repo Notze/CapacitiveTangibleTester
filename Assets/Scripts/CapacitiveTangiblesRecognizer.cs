b5341981e2ed70c9cd20ab6121c2e22b7a84c208
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2018-05-31 01:16:22 +0200

    Density Based Clustering works

--- Assets/Scripts/CapacitiveTangiblesRecognizer.cs
+++ Assets/Scripts/CapacitiveTangiblesRecognizer.cs
@@ -2,39 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
-public class CapacitiveTangiblesRecognizer : MonoBehaviour {
+
+
+public class CapacitiveTangiblesRecognizer : MonoBehaviour
+{
 
     public List<TangiblePattern> patterns;
-    void Start() {
+    public List<DbscanPoint> dbscanPoints = new List<DbscanPoint>();
+    public int clusterCount = 0;
+    public List<Color> clusterColors = new List<Color>();
+    void OnEnable()
+    {
+
+    }
+
+    void Start()
+    {
+
         LoadTangiblesPatterns();
     }
     // Update is called once per frame
-    void Update () {
-        if(Input.touchCount >= 3){
-            
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            clusterCount = DensityBasedClustering.DBScan(dbscanPoints, 1, 3);
+            clusterColors = ClusterColors(clusterCount);
         }
-	}
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            dbscanPoints.Clear();
+        }
+
+        List<Vector2> touchPoints = new List<Vector2>();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            touchPoints.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            RecognizeTangiblesPattern(patterns[0], touchPoints);
+        }
+
+        //if(Input.touchCount >= 3){
+        //    foreach(Touch touch in Input.touches){
+        //        touchPoints.Add(Camera.main.ScreenToWorldPoint(touch.position));
+        //    }
+
+        //    RecognizeTangiblesPattern(patterns[0], touchPoints);
+        //}
+    }
 
     public void LoadTangiblesPatterns()
     {
         patterns = new List<TangiblePattern>();
         string[] filenames = TangiblesFileUtils.LoadTangiblesJSON();
-        foreach(string filename in filenames){
+        foreach (string filename in filenames)
+        {
             string json = System.IO.File.ReadAllText(filename);
             TangiblePattern pattern = JsonUtility.FromJson<TangiblePattern>(json);
             patterns.Add(pattern);
         }
     }
 
+    private void OnDrawGizmos()
+    {
+        
+        foreach (DbscanPoint point in dbscanPoints)
+        {
+            if(point.ClusterId > -1){
+                Gizmos.color = clusterColors[point.ClusterId-1];    
+            }else{
+                Gizmos.color = Color.black;
+            }
 
-    public float RecognizeTangiblesPattern(List<Vector2> tangiblePoints, List<Vector2> touchPoints){
-        float probability = 0;
-        Vector2 touchCenter = MathHelper.ComputeCenter(touchPoints);
-
-        for (int i = 0; i < tangiblePoints.Count; i++){
-            tangiblePoints[i] += touchCenter;
+            Gizmos.DrawSphere(point.point, 0.25f);
         }
 
+    }
+
+    public float RecognizeTangiblesPattern(TangiblePattern pattern, List<Vector2> touchPoints)
+    {
+        //recognizerTouches = new List<RecognizerTouch>();
+        float probability = 0;
+
+        foreach (Vector2 tp in touchPoints)
+        {
+            DbscanPoint dbscanPoint = new DbscanPoint(tp);
+            dbscanPoints.Add(dbscanPoint);
+        }
         return probability;
     }
+
+    List<Color> ClusterColors(int N){
+        List<Color> colors = new List<Color>();
+        float step = 360.0f / N;
+        for (int i = 0; i < N; i++){
+            colors.Add(Color.HSVToRGB((i * step)/360.0f, 1, 1));
+        }
+        return colors;
+    }
+
+
+
 }
---
+++ Assets/Scripts/DensityBasedClustering.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DbscanPoint
+{
+
+    public Vector2 point;
+
+    /// <summary>
+    /// Gets or sets the X component of the point.
+    /// </summary>
+    public float X { get { return point.x; } set { point.x = value; } }
+
+    /// <summary>
+    /// Gets or sets the Y component of the point.
+    /// </summary>
+    public float Y { get { return point.y; } set { point.y = value; } }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the point is noise.
+    /// </summary>
+    public bool IsNoise;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the point was visited.
+    /// </summary>
+    public bool IsVisited;
+
+    /// <summary>
+    /// Gets or sets a value indicating the cluster id.
+    /// </summary>
+    public int ClusterId = -1;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DbscanPoint"/> class.
+    /// </summary>
+    public DbscanPoint(Vector2 p)
+    {
+        point = p;
+    }
+
+    /// <summary>
+    /// Determines whether the specified point neighbors the current instance using the specified value.
+    /// </summary>
+    /// <param name="point">The point to test as a neighor.</param>
+    /// <param name="eps">The value to use to find neighoring points.</param>
+    /// <returns>True if the point is a neighbor; otherwise, false.</returns>
+    public bool IsNeighbor(DbscanPoint point, float eps)
+    {
+        return (Mathf.Pow(point.X - this.X, 2) + Mathf.Pow(point.Y - this.Y, 2) < Mathf.Pow(eps, 2));
+    }
+}
+
+public static class DensityBasedClustering{
+        
+
+    /// <summary>
+    /// Clusters the specified points using the specified value and minimum points to form a cluster.
+    /// </summary>
+    /// <param name="points">The points to cluster.</param>
+    /// <param name="eps">The value to use to find neighoring points.</param>
+    /// <param name="minimumClusterCount">The minimum number of points to form a cluster.</param>
+    /// <returns>The number of clusters created from the collection.</returns>
+    public static int DBScan(List<DbscanPoint> points, float eps, int minimumClusterCount){
+        int cid = 0;
+
+        foreach (DbscanPoint p in points)
+        {
+            if (!p.IsVisited)
+            {
+                p.IsVisited = true;
+
+                DbscanPoint[] neighbors = DensityBasedClustering.GetNeighors(points, p, eps);
+
+                if (neighbors.Length < minimumClusterCount)
+                    p.IsNoise = true;
+                else
+                {
+                    cid++;
+                    DensityBasedClustering.ExpandCluster(points, p, neighbors, cid, eps, minimumClusterCount);
+                }
+            }
+        }
+
+        return cid;
+    }
+
+    private static void ExpandCluster(List<DbscanPoint> points, DbscanPoint p, DbscanPoint[] neighbors, int cid, float eps, int minimumClusterCount){
+        p.ClusterId = cid;
+
+        Queue<DbscanPoint> q = new Queue<DbscanPoint>(neighbors);
+
+        while (q.Count > 0)
+        {
+            DbscanPoint n = q.Dequeue();
+            if (!n.IsVisited)
+            {
+                n.IsVisited = true;
+
+                DbscanPoint[] ns = DensityBasedClustering.GetNeighors(points, n, eps);
+                if (ns.Length >= minimumClusterCount)
+                    foreach (DbscanPoint item in ns)
+                        q.Enqueue(item);
+            }
+            else if (n.ClusterId == -1)
+                n.ClusterId = cid;
+        }
+    }
+
+    private static DbscanPoint[] GetNeighors(List<DbscanPoint> points, DbscanPoint point, float eps)
+    {
+        List<DbscanPoint> neighbors = new List<DbscanPoint>();
+        neighbors.Add(point);
+
+        foreach (DbscanPoint p in points)
+            if (point.IsNeighbor(p, eps))
+                neighbors.Add(p);
+
+        return neighbors.ToArray();
+    }
+}
