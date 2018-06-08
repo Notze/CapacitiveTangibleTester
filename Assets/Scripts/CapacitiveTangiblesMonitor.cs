cefbdc207b067a14e49bdbb15151e5135f542b29
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2018-06-08 19:31:39 +0200

    world corners

--- Assets/Scripts/CapacitiveTangiblesMonitor.cs
+++ Assets/Scripts/CapacitiveTangiblesMonitor.cs
@@ -39,7 +39,7 @@ public class CapacitiveTangiblesMonitor : MonoBehaviour {
 
             //Vector2 sPoint = Camera.main.WorldToScreenPoint(new Vector3(point.x, point.y, 0));
             GameObject touchGO = Instantiate(TouchPrefab);
-            touchGO.transform.position = transform.position + corners[3]/2 + new Vector3 (point.x, point.y, 0);
+            touchGO.transform.position = transform.position + new Vector3 (point.x - corners[0].x, point.y + corners[2].y, 0);
             touchGO.transform.SetParent(this.transform);
             //Vector2 thisPos = new Vector2(rectTransform.position.x - rectTransform.sizeDelta.x/2,
             //                              rectTransform.position.y + rectTransform.sizeDelta.y/2);
