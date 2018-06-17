b7e47ad01e6fc3a55346e81499e1ece3d72e63b8
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2018-06-17 18:56:36 +0200

    global settings now have player prefs

--- Assets/Scripts/GlobalSettings.cs
+++ Assets/Scripts/GlobalSettings.cs
@@ -9,18 +9,53 @@ namespace CTR{
 	}
 
 	public class GlobalSettings : SingletonBehaviour<GlobalSettings> {
+
+		public const string kModality = "kModality";
+		public const string kClusterRadiusScaler = "kClusterRadiusScaler";
+		public const string kAnchorTolerance = "kAnchorTolerance";
+		public const string kPatternFitThreshold = "kPatternFitThreshold";
+		public const string kMinNumOfPointsInCluster = "kMinNumOfPointsInCluster";
+		public const string kFlipRotation = "kFlipRotation";
+		public const string kMinDistanceBetweenTouchPoints = "kMinDistanceBetweenTouchPoints";
+
+		public int numOfClusterPoints = 0;
+		public float rotationAngle;
+		public Text statusText;
+
 		public InputModality modality = InputModality.Touch;
 		public float clusterRadiusScaler = 2.0f;
 		public float anchorTolerance = 0.1f;
 		public float patternFitThreshold = 0.001f;
-		public int numOfclusterPoints = 0;
 		public int minNumOfPointsInCluster = 4;
-		public float rotationAngle;
-		public Text statusText;
 		public bool flipRotation;
 		public float minDistanceBetweenTouchPoints = 0.1f;
-		private void Start ()
-		{
+
+
+		private void Start () {
+
+			if(PlayerPrefs.HasKey(kModality)){
+				modality = (InputModality)PlayerPrefs.GetInt (kModality);	
+			}
+			if (PlayerPrefs.HasKey (kClusterRadiusScaler)) {
+				clusterRadiusScaler = PlayerPrefs.GetFloat(kClusterRadiusScaler);
+			}
+			if (PlayerPrefs.HasKey (kAnchorTolerance)) {
+				anchorTolerance = PlayerPrefs.GetFloat (kAnchorTolerance);
+			}
+			if (PlayerPrefs.HasKey (kPatternFitThreshold)) {
+				patternFitThreshold = PlayerPrefs.GetFloat (kPatternFitThreshold);
+			}
+			if (PlayerPrefs.HasKey (kMinNumOfPointsInCluster)) {
+				minNumOfPointsInCluster = PlayerPrefs.GetInt(kMinNumOfPointsInCluster);
+			}
+			if (PlayerPrefs.HasKey (kFlipRotation)) {
+				flipRotation = PlayerPrefs.GetInt (kFlipRotation) > 0;
+			}
+			if (PlayerPrefs.HasKey (kMinDistanceBetweenTouchPoints)) {
+				minDistanceBetweenTouchPoints = PlayerPrefs.GetFloat(kMinDistanceBetweenTouchPoints);
+			}
+
+
 			SetStatusText ();
 		}
 
@@ -39,7 +74,7 @@ namespace CTR{
 		{
 			statusText.text = string.Format ("modality: {0} cluster points: {1}; cluster radius scalar: {2}; tolerance: {3}; angle: {4}; flip: {5}; min dist: {6}; fitness threshold: {7}; screen DPI {8}",
 											modality,
-											numOfclusterPoints,
+											numOfClusterPoints,
 											clusterRadiusScaler,
 											anchorTolerance,
 											rotationAngle,
@@ -47,6 +82,15 @@ namespace CTR{
 											minDistanceBetweenTouchPoints,
 											patternFitThreshold,
 											Screen.dpi);
+
+
+			PlayerPrefs.SetInt (kModality, (int)modality);
+			PlayerPrefs.SetFloat (kClusterRadiusScaler, clusterRadiusScaler);
+			PlayerPrefs.SetFloat (kAnchorTolerance, anchorTolerance);
+			PlayerPrefs.SetFloat(kPatternFitThreshold, patternFitThreshold);
+			PlayerPrefs.SetFloat (kMinNumOfPointsInCluster, minNumOfPointsInCluster);
+			PlayerPrefs.SetInt (kFlipRotation, flipRotation == true ? 1: 0);
+			PlayerPrefs.SetFloat (kMinDistanceBetweenTouchPoints, minDistanceBetweenTouchPoints);
 		}
 
 		public void SetModality (int m)
@@ -57,7 +101,7 @@ namespace CTR{
 
 		public void SetNumClusterPoints (int numPoints)
 		{
-			numOfclusterPoints = numPoints;
+			numOfClusterPoints = numPoints;
 			SetStatusText ();
 		}
 
