cbf1276c9c72f3f496aea9f16d82d78d6524c52e
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2018-06-17 11:31:35 +0200

    added CTR namespace

--- Assets/Scripts/CapacitiveTangiblesTrainer.cs
+++ Assets/Scripts/CapacitiveTangiblesTrainer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 
-namespace CTR{
+namespace CTR {
 	public class CapacitiveTangiblesTrainer : MonoBehaviour {
 		
 		public Text patternID;
