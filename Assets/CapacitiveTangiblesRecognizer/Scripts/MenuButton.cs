96e24d4c15ccbd7cacfc3c5c3ee2d442cffe5006
nordloh <nordloh@uni-bremen.de>
2019-01-29 23:25:24 +0100

    added functionality for microcontroller tangibles und some new (mostly) raw scenes

---
+++ Assets/CapacitiveTangiblesRecognizer/Scripts/MenuButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour {
+
+	public void changeScene (string scene){
+		Application.LoadLevel (scene);
+	}
+
+	public void changeScene (UnityEngine.UI.Dropdown list){
+		Application.LoadLevel (list.options[list.value].text);
+	}
+}
---
+++ Assets/CapacitiveTangiblesRecognizer/Scripts/OSCReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OSCReceiver : MonoBehaviour {
+
+	public OSC osc;
+	public Text debugText;
+	
+	// Use this for initialization
+	void Start () {
+		osc.SetAddressHandler ("/ID", onReceiveID);
+		debugText.text = "OSC-Receiver has been set up!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		debugText.text = "waiting for signal from tangible";
+	}
+
+	void onReceiveID(OscMessage message){
+		float tangID = message.GetFloat(0);
+		debugText.text = "touched tangible ID: " + tangID.ToString();
+	}
+}
