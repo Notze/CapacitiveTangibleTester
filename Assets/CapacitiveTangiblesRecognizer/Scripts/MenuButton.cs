a43bb3bd97a47cb7b3c193e6cdb00c29aed991fa
nordloh <nordloh@uni-bremen.de>
2019-05-29 16:02:17 +0200

    menu buttons working; added eventsystem on other scenes

--- Assets/CapacitiveTangiblesRecognizer/Scripts/MenuButton.cs
+++ Assets/CapacitiveTangiblesRecognizer/Scripts/MenuButton.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class MenuButton : MonoBehaviour {
 
 	public void changeScene (string scene){
-		Application.LoadLevel (scene);
-	}
+		//Application.LoadLevel (scene); // deprecated
+        UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
+    }
 
 	public void changeScene (UnityEngine.UI.Dropdown list){
 		Application.LoadLevel (list.options[list.value].text);
 	}
+
+    public void closeApplication()
+    {
+        Application.Quit();
+    }
 }
